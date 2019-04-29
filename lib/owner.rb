68b7f0502d572420f728a8c8ae620eae47b8004b
Jasterix C <loune.calixte@gmail.com>
2019-04-29 15:11:32 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
   # code goes here
+  @@count = 0
+  ALL = []
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(species)
+    @species = species
+    ALL << self
+    @@count += 1
+  end
+    
+  def species
+    
+  end
+  
+  def self.all
+    ALL
+  end
+  
+  def self.count
+    @@count
+  end
+  
 end
